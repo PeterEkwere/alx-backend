c42abb8a29e233e23c85be12171e007e0a912358
PeterEkwere <peterekwere@icloud.com>
2024-02-06 23:55:38 +0100

    update 12 for task 0

--- 0x02-i18n/app.py
+++ 0x02-i18n/app.py
@@ -1,16 +1,12 @@
 #!/usr/bin/env python3
-"""
-    This Module contains a basic api route
-    Author: Peter Ekwere
-"""
-from flask import Flask, render_template
+"""module for flask app"""
 
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    """ an index endpoint
-    """
-    return render_template('0-index.html')
+@app.route("/")
+def hello():
+    """renders 0-index.html"""
+    return render_template("0-index.html")
